9e45f7a6f6b623956daff4be3bfbc0e4d5549613
Bryce Hill <bryce@clearvoice.com>
2018-07-02 14:08:30 -0700

    Make request to get payees WIP

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import System.IO
+import Data.Aeson (Value)
+import Data.Aeson.Lens (key, _Value, _String)
+import Control.Lens
+import Network.Wreq (defaults, getWith, header,responseBody, responseStatus, statusCode, Options)
+import Data.ByteString.Char8 (pack)
+import System.Environment (getEnv)
+
+
+baseUrl :: String
+baseUrl = "https://api.youneedabudget.com/v1/"
+
+payeesUrl :: String -> String
+payeesUrl budgetId = baseUrl ++ "budgets/" ++ budgetId ++ "/payees"
+
+opts :: String -> Network.Wreq.Options
+opts token = defaults & header "Authorization" .~ [pack $ "Bearer " ++ token]
+
+getStarbucksPayeeIds :: String -> String -> IO (Maybe Value)
+getStarbucksPayeeIds token budgetId = do
+  r <- getWith (opts token) (payeesUrl budgetId)
+  return (r ^? responseBody . key "data" . key "payees")
+
+
 
 main :: IO ()
-main = someFunc
+main = do
+  token <- getEnv("YNAB_ACCESS_TOKEN")
+  budgetId <- getEnv("YNAB_BUDGET_ID")
+  payeeIds <- getStarbucksPayeeIds token budgetId
+  return ()
