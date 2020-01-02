6b126c5b95110d066388934c902f7086112619ed
Bryce Hill <bryce@clearvoice.com>
2020-01-02 09:34:05 -0700

    WIP move script to haskell

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,18 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 
 module Main where
 
 import System.IO
-import Data.Aeson (Value)
-import Data.Aeson.Lens (key, _Value, _String)
+import GHC.Generics
+-- improt Data.Map as Map
+import Data.Aeson (Value, FromJSON(..), withObject, withArray, (.:))
+import Data.Aeson.Lens (key, _Value, _String, _Array, _Object)
 import Control.Lens
-import Network.Wreq (defaults, getWith, header,responseBody, responseStatus, statusCode, Options)
-import Data.ByteString.Char8 (pack)
+import Network.Wreq (
+  asJSON,
+  defaults, getWith, header,responseBody, responseStatus, statusCode,
+  Options, Response)
+import Data.ByteString.Char8 (ByteString, pack)
 import System.Environment (getEnv)
 
 
@@ -20,16 +25,60 @@ payeesUrl budgetId = baseUrl ++ "budgets/" ++ budgetId ++ "/payees"
 opts :: String -> Network.Wreq.Options
 opts token = defaults & header "Authorization" .~ [pack $ "Bearer " ++ token]
 
-getStarbucksPayeeIds :: String -> String -> IO (Maybe Value)
-getStarbucksPayeeIds token budgetId = do
-  r <- getWith (opts token) (payeesUrl budgetId)
-  return (r ^? responseBody . key "data" . key "payees")
+
+data Payee = Payee { name :: String }
+ deriving (Show)
+
+
+data Payees = List Payee
+  deriving (Show, Generic)
+
+-- Idea: typeclass that implements the way to return
+-- data.payees/budgets?
+
+instance FromJSON Payees where
+  parseJSON = withObject "YNABResp" $ \json -> do
+    body <- json .: "data"
+    ps <- body .: "payees"
+    return $ Payees (map (\p -> Payee (name p)) ps)
+
+-- instance FromJSON Payee where
+--   parseJSON = withArray "Payee" $ \json -> do
+--     return $ Payee 
+
+
+getPayees :: String -> String -> IO ()
+getPayees token budgetId = do
+  r <- asJSON =<< getWith (opts token) (payeesUrl budgetId)
+  print $ body (r ^. responseBody)
+  -- findSbuxIds r
+  -- TODO: Handle errors
+  -- putStr $ show ()
+  return ()
+
+-- getStarbucksPayeeIds :: String -> String -> IO ()
+-- getStarbucksPayeeIds token budgetId = do
+--   r <- asJSON =<< getWith (opts token) (payeesUrl budgetId)
+--   findSbuxIds r
+--   -- TODO: Handle errors
+--   -- putStr $ show ()
+--   return ()
+
+-- findStarbucks
+-- const getSbuxPayeeIds = compose(
+--   pluck('id'),
+--   filterName(/starbucks/i),
+-- )
+
+findSbuxIds :: Payees -> [String]
+findSbuxIds ps = map (name) ps
+
 
 
 
 main :: IO ()
 main = do
-  token <- getEnv("YNAB_ACCESS_TOKEN")
-  budgetId <- getEnv("YNAB_BUDGET_ID")
-  payeeIds <- getStarbucksPayeeIds token budgetId
+  token    <- getEnv ("YNAB_ACCESS_TOKEN")
+  budgetId <- getEnv ("YNAB_BUDGET_ID")
+  payeeIds <- getPayees token budgetId
   return ()
